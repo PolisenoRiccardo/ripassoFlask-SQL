fae807d202e1b7e9c62ee8b6ca0fdec005ccee60
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-04-29 13:38:26 +0000

    save

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    
+    import pyodbc
+    import pandas as pd
+
+    server = '5.172.64.20'
+    database = 'zhao.filippo'
+    username = 'zhao.filippo'
+    password = 'xxx123##'
+    driver= '{SQL Server}' # controllare che il driver ODBC sia installato sulla macchina in cui è in esecuzione l'interprete Python
+
+              
+    connectionString = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+    conn = pyodbc.connect(connectionString) 
+
+    sql_query = """
+    SELECT * FROM sales.staffs
+    """
+    
+    df = pd.read_sql(sql_query, conn)
+    return render_template('table.html', tabella = df.to_html())
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
