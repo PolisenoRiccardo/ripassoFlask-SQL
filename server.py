7102d57b7e019c4c931608c0149d6e35ada7c790
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-04-29 15:53:00 +0200

    Schema Poliseno

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def hello_world():
+@app.route('/', methods=['GET']) 
+def table():
     
     import pyodbc
     import pandas as pd
@@ -11,18 +11,18 @@ def hello_world():
     database = 'zhao.filippo'
     username = 'zhao.filippo'
     password = 'xxx123##'
-    driver= '{SQL Server}' # controllare che il driver ODBC sia installato sulla macchina in cui è in esecuzione l'interprete Python
+    driver= '{SQL Server}' # il driver ODBC deve essere installato sulla macchina in cui è in esecuzione l'interprete Python
 
               
     connectionString = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
     conn = pyodbc.connect(connectionString) 
 
     sql_query = """
-    SELECT * FROM sales.staffs
+    SELECT * FROM Poliseno.Videogioco
     """
     
     df = pd.read_sql(sql_query, conn)
     return render_template('table.html', tabella = df.to_html())
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+  app.run(host='0.0.0.0', port=3245, debug=True) 
